8ebb09f719e9c34532e560e87e0c71d539c481ae
Tran-Duc-Viet <102152095+Tran-Duc-Viet@users.noreply.github.com>
2022-06-07 00:34:46 +0700

    Update Assigment2.sql

--- Assigment2.sql
+++ Assigment2.sql
@@ -1,10 +1,11 @@
+--Tạo bảng không sử dụng Index, dùng clustered index và dùng non clustered index.
 Select * INTO dbo.DimReseller_NoIndex FROM dbo.DimReseller;
 Select * INTO dbo.DimReseller_ClusteredIndex FROM dbo.DimReseller;
 Select * INTO dbo.DimReseller_NonClusteredIndex FROM dbo.DimReseller;
-
+--Tạo index
 CREATE INDEX Idx_DimReseller_ResellerAlternateKey ON dbo.DimReseller_NonClusteredIndex(ResellerAlternateKey);
 CREATE CLUSTERED INDEX Idx_DimReseller_ResellerAlternateKey ON dbo.DimReseller_ClusteredIndex(ResellerAlternateKey);
-
+--Câu lệnh chạy để so sánh:
 SELECT * FROM dbo.DimReseller_NoIndex WHERE ResellerAlternateKey='AW00000174';
 SELECT * FROM dbo.DimReseller_ClusteredIndex WHERE ResellerAlternateKey='AW00000174';
 SELECT * FROM dbo.DimReseller_NonClusteredIndex WHERE ResellerAlternateKey='AW00000174';
